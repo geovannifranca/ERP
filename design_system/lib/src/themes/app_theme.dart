305b91f8b5a95a4af593fb848a72d639e3f146c2
geovannifranca <geovannidefranca98@gmail.com>
2024-02-02 21:30:47 -0300

    first commit

---
+++ design_system/lib/src/themes/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:design_system/src/themes/dark/dark_theme.dart';
+import 'package:design_system/src/themes/light/light.theme.dart';
+
+class AppTheme {
+  static final light = LightTheme.theme;
+  static final dart = DarkTheme.theme;
+}
